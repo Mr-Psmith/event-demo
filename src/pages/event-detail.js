4f9018029f6fd55737863db22c6254caed3760e0
Mr-Psmith <psancthus@gmail.com>
2023-07-06 14:35:51 +0000

    loader

--- src/pages/event-detail.js
+++ src/pages/event-detail.js
@@ -1,14 +1,19 @@
 import { useParams } from 'react-router-dom';
+import EventItem from '../components/EventItem';
 
 function EventDetailPage() {
   const params = useParams();
 
   return (
     <>
-      <h1>EventDetailPage</h1>
-      <p>Event ID: {params.eventId}</p>
+      <EventItem event={} />
     </>
   );
 }
 
 export default EventDetailPage;
+
+export async function loader({request, params}) {
+  
+  fetch("http://localhost:8080/events/");
+}
