b6011ec84192c3b8f96d3adc599eadccc64e1896
Mr-Psmith <psancthus@gmail.com>
2023-07-21 10:48:14 +0000

    I Await the defer in Suspense and else

--- src/pages/event-detail.js
+++ src/pages/event-detail.js
@@ -1,32 +1,72 @@
-import { Link, json, redirect, useRouteLoaderData } from 'react-router-dom';
+import { Await, Link, defer, json, redirect, useRouteLoaderData } from 'react-router-dom';
 import EventItem from '../components/EventItem';
+import EventsList from '../components/EventsList';
+import { Suspense } from 'react';
 
 function EventDetailPage() {
-  const data = useRouteLoaderData("event-detail");
+  const {event, events} = useRouteLoaderData("event-detail");
 
   //const params = useParams();
 
   return (
     <>
-      <EventItem event={data.event} />
-
+      <Suspense fallback={<p style={{textAlign: "center"}}>Loading....</p>}>
+        <Await resolve={event}>
+          {loadedEvent => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+      <Suspense fallback={<p style={{textAlign: "center"}}>Loading....</p>}>
+        <Await resolve={events}>
+          {loadedEvents => <EventsList events={loadedEvents}/>}
+        </Await>
+      </Suspense>
       <p><Link to=".." relative='path'>Back</Link></p>
     </>
   );
 }
 
 export default EventDetailPage;
+
+
+async function loadEvent(id) {
+  const response = await fetch("http://localhost:8080/events/" + id);
+  if (!response.ok) {
+    throw json({message: "Could not fetch details for selected event."}, { status: 500})
+  } else {
+    const resData = await response.json();
+    return resData.event;
+  }
+}
+//copyed from events.js bec of defer controlling 
+async function loadEvents() { 
+  const response = await fetch('http://localhost:8080/events');
+            if (!response.ok) {
+              return json({message: "could not fetch events."},
+              { status: 500 });
+            } else {
+              const resData = await response.json();
+              return resData.events;     
+            }
+};
+
                                   //react router, passes an object to this loader function when executing it which contains two important pieces of data: 
                                     //A request property, which contains a request object, and a params property, which contains an object with all your route parameters.
 export async function loader({request, params}) { //this is to load the event, which we could do with param and useEffect, or with this loader in the app.js(I think), or like this
   const id = params.eventId; //from the path from app.js if I get it well. 
 
-  const response = await fetch("http://localhost:8080/events/" + id);
-  if (!response.ok) {
-    throw json({message: "Could not fetch details for selected event."}, { status: 500})
-  } else {
-    return response;
-  }
+  return defer({
+    event: await loadEvent(id), //having async loader with the async function, simply adding the await keyword here, will make sure that defer waits for this data to be loaded before loading this page component at all
+    events: loadEvents()          //So await can be a lever - switch for controlling which data should be awaited before moving to this page, and which data should be deferred
+  });
+
+
+  //the below code we put abowe in the loadEvent funct bec of defer
+  // const response = await fetch("http://localhost:8080/events/" + id);
+  // if (!response.ok) {
+  //   throw json({message: "Could not fetch details for selected event."}, { status: 500})
+  // } else {
+  //   return response;
+  // }
 }
 
 export async function action({params, request}) {
@@ -43,3 +83,4 @@ export async function action({params, request}) {
 }
 
 
+//git commit -m "I Await the defer in Suspense" 
--- src/pages/events.js
+++ src/pages/events.js
@@ -1,34 +1,49 @@
-import { json, useLoaderData } from 'react-router-dom';
+import { Await, defer, json, useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
+import { Suspense } from 'react';
 
 function EventsPage() {
-const data = useLoaderData();
+const {events} = useLoaderData();
 
 //if (data.isError) {//so if the isError is truthy. Which I dont understadn, that why react understands here the data. part. Where does it come from?
  // return <p>{data.message}</p> //than we output the message set below. okeay this as well, only that WHERE DOUS THE DATA COME FROM. AND WHY IsNT MAX MENTIONING THIS?
 //}
-const events = data.events;
+//const events = data.events;
 
  return (
-   <EventsList events={events} />
+  //  <EventsList events={events} /> Bec of DEFER() we dont directly return this here, but:
+  <Suspense fallback={<p style={{textAlign: "center"}}>Loading....</p>}> {/* component which can be used in certain situations to show a fallback whilst we're waiting for other data to arrive. */}
+    <Await resolve={events} >
+      {(loadedEventsPr) => <EventsList events={loadedEventsPr} />}
+    </Await> {/* //And await has a special resolve prop which wants one of our deferred values as a value. */}
+  </Suspense>
   );
 }
 
 export default EventsPage;
 
-export async function loaderUpToUs() {
-    const response = await fetch('http://localhost:8080/events');
+async function loadEvents() { //outsourcing the code from loaderfunctioninto into this separate function, into an async function, bec in the loader don't want to await promise  
+  const response = await fetch('http://localhost:8080/events');   //instead we get rid of this async keyword and use a special function - The defer function.
             if (!response.ok) {
               //1return {isError: true, message: "Could not get any events."} this was too simple
               //2throw new Response(JSON.stringify({message: "could not fetch events."}), {status: 500} ); //when an error gets thrown in a loader react-router has a special reaction for this: it renders the closest error element.
                                     //we  can throw a new Response as well, and we need to call JSON.stringify if we want on obj there. /ans status: 500 shows that we have what kind of problem
               return json({message: "could not fetch events."},  //this is the same as abowe, except with less code, and we dont have to parse it in the error.js file
               { status: 500 });
-            } else {     
-                return response; //But we can just return the response like this and useLoaderData will then automatically give us the data that's part of the response.
+            } else {
+              //Have to manually parse bec of defer()
+              const resData = await response.json();
+              return resData.events;     
+                //return response; //But we can just return the response like this and useLoaderData will then automatically give us the data that's part of the response.
                 // const resData = await response.json();
                 // return resData.events;
                 // const res = new Response("any data", {status: 201}); // we can configure it with help of an extra object that can be set as a second argument.
                 // return res;
             }
-          };
+};
+
+export function loaderUpToUs() {
+    defer({
+      events: loadEvents(), //loadEvents returns a promise - so data isnt there yet - store that promise under the events key in this object which we pass to defer.
+    });
+  };
